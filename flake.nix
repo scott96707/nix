684dc0be9c670ce4420dfd165423d4b441f2c2c9
scott96707 <scott96707@gmail.com>
2025-12-18 13:37:43 -0700

    feat: Creating hosts/macbook/home.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,22 @@
 {
-  description = "Home NixOS Configuration";
+  description = "Home Nix Desktop & Macbook Configuration";
 
   inputs = {
+    # 1. Global Nixpkgs (Pinned to Stable 24.11)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
+    # 2. Home Manager
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # 3. Darwin (MacOS System Config)
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nix-darwin, ... }@inputs: {
     
-    # --- SYSTEM CONFIGURATION (Root) ---
-    # Run with: sudo nixos-rebuild switch --flake .#nixos
+    # --- LINUX PC (NixOS) ---
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
@@ -20,17 +25,29 @@
       ];
     };
 
-    # --- USER CONFIGURATION (Home) ---
-    # Run with: home-manager switch --flake .#home
+    # --- MACBOOK (Darwin) ---
+    # Build with: darwin-rebuild switch --flake .#macbook
+    darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
+      system = "x86_64-darwin"; 
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/macbook/system.nix
+        
+        # Setup Home Manager as a module for the Mac
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.scott = import ./hosts/macbook/home.nix;
+        }
+      ];
+    };
+
+    # --- STANDALONE HOME (Linux) ---
     homeConfigurations."home" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      
-      # Pass inputs so you can access them in home.nix if needed
       extraSpecialArgs = { inherit inputs; };
-      
-      modules = [
-        ./hosts/nixos/home.nix
-      ];
+      modules = [ ./hosts/nixos/home.nix ];
     };
   };
 }
